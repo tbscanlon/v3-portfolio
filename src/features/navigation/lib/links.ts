a2c0abfcc32f2dec17816e3c3d3614c79666124c
Tom Scanlon <thomas.scanlon@outlook.com>
2023-09-26 15:21:43 +0100

    First pass of footer

---
+++ src/features/navigation/lib/links.ts
@@ -0,0 +1,23 @@
+interface Link {
+  text: string;
+  href: string;
+}
+
+export const links: Link[] = [
+  {
+    href: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    href: "/blog",
+    text: "Blog",
+  },
+  {
+    href: "/about",
+    text: "About",
+  },
+  {
+    href: "/contact",
+    text: "Contact",
+  },
+];
