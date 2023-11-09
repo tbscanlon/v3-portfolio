2469bb20d797dafc4dd7a206ec181325fc0474e7
Tom Scanlon <thomas.scanlon@outlook.com>
2023-11-09 17:07:40 +0100

    Try to sort dialog layout and interest types

--- src/features/interests/lib/types.d.ts
+++ src/features/interests/lib/types.d.ts
@@ -14,3 +14,9 @@ export interface Interest {
   author?: string;
   genres?: string;
 }
+
+export type Listening = Pick<Interest, "type" | "url" | "title" | "artist">;
+export type Playing = Pick<Interest, "type" | "url" | "title" | "developer">;
+export type Reading = Pick<Interest, "type" | "url" | "title" | "author">;
+export type Studying = Pick<Interest, "type" | "url" | "title" | "author">;
+export type Watching = Pick<Interest, "type" | "url" | "title" | "genres">;
