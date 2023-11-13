0d1e1cbbd283d8bb541f9c1ce145da294fd921f1
Tom Scanlon <thomas.scanlon@outlook.com>
2023-11-13 13:42:27 +0100

    Add more info to projects content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,17 +19,29 @@ const interestsCollection = defineCollection({
 
 const projectsCollection = defineCollection({
   type: "data",
-  schema: z.array(
-    z.object({
-      slug: z.string(),
-      title: z.string(),
-      url: z.string().url(),
-      image: z.string(),
-      description: z.string(),
-      longDescription: z.array(z.string()),
-      stack: z.array(z.string()),
-    })
-  ),
+  schema: z.object({
+    title: z.string(),
+    introduction: z.string(),
+    cta: z
+      .object({
+        text: z.string(),
+        url: z.string(),
+      })
+      .optional(),
+    projects: z.array(
+      z.object({
+        slug: z.string(),
+        title: z.string(),
+        url: z.string().url(),
+        image: z.string(),
+        description: z.string(),
+        longDescription: z.array(z.string()),
+        stack: z.array(z.string()),
+        startDate: z.string().datetime(),
+        endDate: z.string().datetime().optional(),
+      })
+    ),
+  }),
 });
 
 export const collections = {
