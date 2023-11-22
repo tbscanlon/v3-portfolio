3b7a3ec7064e70d883dec5660edcbc742199127f
Tom Scanlon <thomas.scanlon@outlook.com>
2023-11-22 13:25:37 +0100

    Revert "Install SolidJS"
    This reverts commit f2a87ffd27db8c537d687cc163a506fe55e58fea.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
-import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), solidJs()]
+  integrations: [tailwind(), mdx()]
 });
